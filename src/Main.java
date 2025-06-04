88332f0af8529e79fd1a2de94c1a1910d35c4e82
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-04 14:53:26 +0900

    fourth for-while

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        boolean a = true;
-        int b = 1;
+        int input = 1;
 
-        while (a) {
-            System.out.println(b);
-            b++;
+        do {
+            System.out.println("안녕하세요 자판기 입니다.");
 
-            if (b > 10000) {
-                a = false;
+            switch (input) {
+                case 1:
+                    System.out.println("콜라");
+                    break;
+                case 2:
+                    System.out.println("사이다");
+                    break;
+                case 3:
+                    System.out.println("물");
+                    break;
+                default:
+                    System.out.println("잘못 선택하셨습니다.");
+
             }
-        }
+        } while (input == 0);
     }
 }
